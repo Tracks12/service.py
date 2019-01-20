6472e3b63fac9f8519d911fd6257028a39667165
Anarchy <cardinal.florian.erwan@gmail.com>
2019-01-20 03:36:56 +0100

    Add files via upload

--- service.py
+++ service.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
 
-# (c) 2017 - Copyright by 4N4RCHY. All rights is reserved.
+# ----------------------
+#  Autor   : Anarchy
+#  Date    : 20/01/2019
+#  Name    : service.py
+#  Version : 0.0.3-a
+# ----------------------
 
-# --------------------------------------
-#  Program >>> service.py
-#  Function >>> Show & Control Services
-#  Version >>> 0.0.2-a
-# --------------------------------------
-
-import os, platform, sys, warnings
+import os, platform, sys
 from Tkinter import *
 
 class color:
@@ -27,32 +26,32 @@ class color:
 
 def madeButton(panel, act):
 	for i, txt in enumerate(["START", "STOP", "RESTART"]):
-		Button(panel, text=txt, command=act[i]).pack(side=LEFT, padx=6, pady=4)
+		Button(panel, text=txt, command=act[i]).grid(row=i, column=1, padx=6, pady=4, sticky=W)
 		i += 1
 
 def madeLabel(panel, labels):
 	for i, txt in enumerate(labels):
-		Label(panel, text=txt).pack(pady=1)
+		Label(panel, text=txt).grid(pady=1)
 		i += 1
 
-def madePanel(panel, panelName, act):
+def madePanel(panel, panelName, r, act):
 	subpanel = Frame(panel, bd=1, relief=GROOVE)
-	subpanel.pack(side=TOP, padx=8, pady=8)
-	Label(subpanel, text=panelName).pack(padx=5, pady=2)
+	subpanel.grid(row=r, column=0, padx=8, pady=8)
+	Label(subpanel, text=panelName).grid(row=0, column=0, padx=5, pady=2)
 	madeButton(subpanel, act)
 
 def editApache():
-	print("> " + color.YELLOW + "Editing Apache2 Configuration File_" + color.END)
+	print(name + "> " + color.YELLOW + "Editing Apache2 Configuration File_" + color.END)
 	os.system("nano /etc/apache2/apache2.conf")
 	check()
 
 def editMysql():
-	print("> " + color.YELLOW + "Editing MySQL Configuration File_" + color.END)
+	print(name + "> " + color.YELLOW + "Editing MySQL Configuration File_" + color.END)
 	os.system("nano /etc/mysql/my.cnf")
 	check()
 	
 def editTor():
-	print("> " + color.YELLOW + "Editing Tor Configuration File_" + color.END)
+	print(name + "> " + color.YELLOW + "Editing Tor Configuration File_" + color.END)
 	os.system("nano /etc/tor/torsocks.conf")
 	check()
 
@@ -61,21 +60,21 @@ def apacheError(): os.system("cat /var/log/apache2/error.log")
 
 def torLog(): os.system("cat /var/log/tor/log")
 
-def apacheStart(): os.system(apacheService+'start'); check()
-def apacheRestart(): os.system(apacheService+'restart'); check()
-def apacheStop(): os.system(apacheService+'stop'); check()
+def apacheStart(): os.system(service[0]+'start'); check()
+def apacheRestart(): os.system(service[0]+'restart'); check()
+def apacheStop(): os.system(service[0]+'stop'); check()
 
-def mysqlStart(): os.system(mysqlService+'start'); check()
-def mysqlRestart(): os.system(mysqlService+'restart'); check()
-def mysqlStop(): os.system(mysqlService+'stop'); check()
+def mysqlStart(): os.system(service[1]+'start'); check()
+def mysqlRestart(): os.system(service[1]+'restart'); check()
+def mysqlStop(): os.system(service[1]+'stop'); check()
 
-def torStart(): os.system(torService+'start'); check()
-def torRestart(): os.system(torService+'restart'); check()
-def torStop(): os.system(torService+'stop'); check()
+def torStart(): os.system(service[2]+'start'); check()
+def torRestart(): os.system(service[2]+'restart'); check()
+def torStop(): os.system(service[2]+'stop'); check()
 
 def startAll():
-	apacheStart();
-	mysqlStart();
+	apacheStart()
+	mysqlStart()
 	if(tor): torStart()
 
 def restartAll():
@@ -88,12 +87,12 @@ def stopAll():
 	mysqlStop()
 	if(tor): torStop()
 
-def check(): print("> Action:\t\t\t\t[" + color.B_GREEN + " TERMINATED " + color.END + "]")
+def check(): print(name + "> Action:\t\t\t\t[" + color.B_GREEN + " TERMINATED " + color.END + "]")
 
 def listProject():
 	racine = "/var/www/html"; output = ""
 	
-	print("> Listing Project in " + color.ITALIC + racine + color.END + "\n")
+	print(name + "> " + color.YELLOW + "Listing Project in " + color.END + color.ITALIC + racine + color.END + "\n")
 	for i, txt in enumerate(os.listdir(racine)):
 		s = ""
 		if("." in txt): col = color.PURPLE;
@@ -107,7 +106,7 @@ def listProject():
 	print(output)
 
 def about():
-	print("> " + color.YELLOW + "Show more info_" + color.END)
+	print(name + "> " + color.YELLOW + "Show more info_" + color.END)
 	aboutus = Tk()
 	aboutus.title('Service { ABOUT }')
 	aboutus.resizable(width=FALSE, height=FALSE)
@@ -124,7 +123,7 @@ def about():
 	aboutus.mainloop()
 
 def helper():
-	print("> " + color.YELLOW + "Show helper_" + color.END)
+	print(name + "> " + color.YELLOW + "Show helper_" + color.END)
 	helper = Tk()
 	helper.title('Service { HELPER }')
 	
@@ -141,35 +140,38 @@ def helper():
 	helper.mainloop()
 
 def program():
+	global name, service, tor, version
+	
 	os.system("clear")
-	print("> " + color.GREEN + "Runing Service Command GRAPH mod_" + color.END)
-	if(tor): print("> " + color.YELLOW + "Tor mod enabled_" + color.END)
+	print(name + "> " + color.GREEN + "Runing Service Command GRAPH mod_" + color.END)
+	if(tor): print(name + "> " + color.YELLOW + "Tor mod enabled_" + color.END)
 	print("")
 	os.system("screenfetch")
 	print("")
 	
-	global apacheService, mysqlService, torService
-	apacheService = "sudo /etc/init.d/apache2 "
-	mysqlService = "sudo /etc/init.d/mysql "
-	if(tor): torService = "sudo /etc/init.d/tor "
+	service = [
+		"sudo /etc/init.d/apache2 ",
+		"sudo /etc/init.d/mysql ",
+		"sudo /etc/init.d/tor "
+	]
 	
-	service = Tk()
-	service.title('Service { COMMAND PANEL }')
-	service.resizable(width=FALSE, height=FALSE)
+	window = Tk()
+	window.title('Service { COMMAND PANEL }')
+	window.resizable(width=FALSE, height=FALSE)
 	
 	""" Menu """
-	menubar = Menu(service, bd=0)
+	menubar = Menu(window, bd=0)
 	
 	menu0 = Menu(menubar, tearoff=0)
-	menu0.add_command(label="Quitter", command=service.quit)
+	menu0.add_command(label="Quitter", command=window.quit)
 	menubar.add_cascade(label="Fichier", menu=menu0)
 	
 	menu1 = Menu(menubar, tearoff=0)
 	menu1.add_command(label="Démarrer Apache2", command=apacheStart)
-	menu1.add_command(label="Démarrer MySQL", command=mysqlStart)
+	menu1.add_command(label="Redémarrer Apache2", command=apacheRestart)
+	menu1.add_command(label="Arrêter Apache2", command=apacheStop)
 	menu1.add_separator()
 	menu1.add_command(label="Configurer Apache2", command=editApache)
-	menu1.add_command(label="Configurer MySQL", command=editMysql)
 	menu1.add_separator()
 	menu1.add_command(label="Voir Access.log", command=apacheAccess)
 	menu1.add_command(label="Voir Error.log", command=apacheError)
@@ -177,66 +179,76 @@ def program():
 	menu1.add_command(label="Lister les Projets", command=listProject)
 	menubar.add_cascade(label="Serveur", menu=menu1)
 	
-	if(tor):
-		menu2 = Menu(menubar, tearoff=0)
-		menu2.add_command(label="Démarrer Tor", command=torStart)
-		menu2.add_separator()
-		menu2.add_command(label="Configurer Tor", command=editTor)
-		menu2.add_separator()
-		menu2.add_command(label="Voir Tor.log", command=torLog)
-		menubar.add_cascade(label="Tor", menu=menu2)
+	menu2 = Menu(menubar, tearoff=0)
+	menu2.add_command(label="Démarrer MySQL", command=mysqlStart)
+	menu2.add_command(label="Redémarrer MySQL", command=mysqlRestart)
+	menu2.add_command(label="Arrêter MySQL", command=mysqlStop)
+	menu2.add_separator()
+	menu2.add_command(label="Configurer MySQL", command=editMysql)
+	menubar.add_cascade(label="Base de Données", menu=menu2)
 	
-	menu3 = Menu(menubar, tearoff=0)
-	menu3.add_command(label="Aide", command=helper)
-	menu3.add_command(label="A propos du soft", command=about)
-	menubar.add_cascade(label="Plus", menu=menu3)
+	if(tor):
+		menu3 = Menu(menubar, tearoff=0)
+		menu3.add_command(label="Démarrer Tor", command=torStart)
+		menu3.add_command(label="Configurer Tor", command=editTor)
+		menu3.add_separator()
+		menu3.add_command(label="Voir Tor.log", command=torLog)
+		menubar.add_cascade(label="Tor", menu=menu3)
+	
+	menu4 = Menu(menubar, tearoff=0)
+	menu4.add_command(label="Aide", command=helper)
+	menu4.add_command(label="A propos du soft", command=about)
+	menubar.add_cascade(label="Plus", menu=menu4)
 	""" ---------------------------------------------------------------------------------- """
 	
 	""" General Control COMMAND """
-	madePanel(service, "General Service", [startAll, stopAll, restartAll])
+	madePanel(window, "General Service", 0, [startAll, stopAll, restartAll])
 	""" ---------------------------------------------------------------------------------- """
 	
 	""" Main Panel """
-	panel1 = Frame(service, bd=1, relief=GROOVE)
-	panel1.pack(side=TOP, padx=8, pady=8)
+	panel1 = Frame(window, bd=1, relief=GROOVE)
+	panel1.grid(row=0, column=1, padx=8, pady=8)
 	
 	# Apache2 Service COMMAND
-	madePanel(panel1, "Service Apache2", [apacheStart, apacheStop, apacheRestart])
+	madePanel(panel1, "Service Apache2", 0, [apacheStart, apacheStop, apacheRestart])
 	
 	# MySQL Service COMMAND
-	madePanel(panel1, "Service MySQL", [mysqlStart, mysqlStop, mysqlRestart])
+	madePanel(panel1, "Service MySQL", 1, [mysqlStart, mysqlStop, mysqlRestart])
 	
 	#Tor Service COMMAND
-	if(tor): madePanel(panel1, "Service Tor", [torStart, torStop, torRestart])
+	if(tor): madePanel(panel1, "Service Tor", 2, [torStart, torStop, torRestart])
 	""" ---------------------------------------------------------------------------------- """
 	
-	Button(service, text="Lister les Projets", command=listProject).pack(side=LEFT, padx=8, pady=8)
-	Button(service, text="QUIT", command=service.quit).pack(side=RIGHT, padx=8, pady=8)
+	Button(window, text="Lister les Projets", command=listProject).grid(row=1, column=0, padx=8, pady=8)
+	Button(window, text="QUIT", command=window.quit).grid(row=1, column=1, padx=8, pady=8, sticky=E)
 	
-	service.config(menu=menubar)
-	service.mainloop()
-	print("> " + color.RED + "Quitting_" + color.END)
-	service.destroy()
+	window.config(menu=menubar)
+	window.mainloop()
+	window.destroy()
+	
+	print(name + "> " + color.RED + "Quitting_" + color.END)
 
-global version, tor
-version = "v_0.0.2-a"
+name = 'Service'
 tor = False
+version = "v_0.0.3-a"
 
 arg = [
 	["-h" in sys.argv, "-?" in sys.argv, "--help" in sys.argv],
-	["-v" in sys.argv, "--version" in sys.argv],
-	["-t" in sys.argv, "--tor" in sys.argv]
+	["-l" in sys.argv, "--list" in sys.argv],
+	["-t" in sys.argv, "--tor" in sys.argv],
+	["-v" in sys.argv, "--version" in sys.argv]
 ]
 
 if(True in arg[0]):
 	print(" python2 service.py\n")
 	print(" Option         Option longue GNU       Description")
 	print(" -h, -?         --help                  Affiche ce message")
-	print(" -v             --version               Affiche la version du soft")
+	print(" -l             --list                  Liste tous le repertoire du serveur")
 	print(" -t             --tor                   Lancement en mod Tor")
+	print(" -v             --version               Affiche la version du soft\n")
 
-elif(True in arg[1]):
-	print(" Version: " + version + "\n")
+elif(True in arg[1]): listProject()
+elif(True in arg[3]): print(" Version: " + version + "\n")
 
 else:
 	if(platform.system() == "Linux"):
