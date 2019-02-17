00aca9e464c80ae41bcef626333231097ffdaf87
Anarchy <cardinal.florian.erwan@gmail.com>
2019-02-17 11:41:51 +0100

    Add files via upload

--- core/color.py
+++ core/color.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 class color:
-	BOLD	= '\033[1m'
-	ITALIC	= '\033[3m'
+	BOLD       = '\033[1m'
+	ITALIC     = '\033[3m'
 	
 	GRAY       = '\033[30m'
 	RED        = '\033[31m'
@@ -19,12 +19,12 @@ class color:
 	GREEN_HL   = '\033[42m'
 	BROWN_HL   = '\033[43m'
 	BLUE_HL    = '\033[44m'
-	MAGENTA_HL = '\033[45m'
+	PURPLE_HL  = '\033[45m'
 	CYAN_HL    = '\033[46m'
 	GRAY_HL    = '\033[47m'
 	WHITE_HL   = '\033[48m'
 	
-	END	= '\033[0m'
+	END        = '\033[0m'
 
 """
 	-----
